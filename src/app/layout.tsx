a6c0b4684c3c86734f2dd962525911b571598794
devpikapequena-svg <devpikapequena@gmail.com>
2025-12-29 23:27:45 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ import { CartProvider } from '@/context/CartContext'
 import { PaymentProvider } from '@/context/PaymentContext'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+
 const META_PIXEL_ID = '868874045516079'
+const TIKTOK_PIXEL_ID = 'D59ICU3C77U7482963B0'
 
 function LayoutContent({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
@@ -33,13 +35,6 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
       handleLoad()
     } else {
       window.addEventListener('load', handleLoad)
-      const observer = new MutationObserver(() => {
-        if (document.readyState === 'complete') {
-          handleLoad()
-          observer.disconnect()
-        }
-      })
-      observer.observe(document.body, { childList: true, subtree: true })
     }
 
     return () => {
@@ -50,8 +45,17 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (isNotFoundPage) return
-    if (typeof window !== 'undefined' && typeof (window as any).fbq === 'function') {
-      ;(window as any).fbq('track', 'PageView')
+
+    if (typeof window !== 'undefined') {
+      // Meta PageView SPA
+      if (typeof (window as any).fbq === 'function') {
+        ;(window as any).fbq('track', 'PageView')
+      }
+
+      // TikTok PageView SPA
+      if (typeof (window as any).ttq === 'function') {
+        ;(window as any).ttq.page()
+      }
     }
   }, [pathname, searchParams, isNotFoundPage])
 
@@ -75,12 +79,17 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
   )
 }
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
         <title>Pelúcia Menino Jesus que Respira | Acalma a Alma</title>
-        <meta name="description" content="Pelúcia Menino Jesus que Respira | Acalma a Alma" />
+        <meta
+          name="description"
+          content="Pelúcia Menino Jesus que Respira | Acalma a Alma"
+        />
         <meta name="robots" content="index, follow" />
 
         {/* ================================
@@ -117,15 +126,68 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
+            t=b.createElement(e);t.async=!0;t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
             fbq('init', '${META_PIXEL_ID}');
             fbq('track', 'PageView');
           `}
         </Script>
 
+        {/* ================================
+            TikTok Pixel
+        ================================= */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;
+              var ttq=w[t]=w[t]||[];
+              ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+              ttq.setAndDefer=function(t,e){
+                t[e]=function(){
+                  t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+                }
+              };
+              for(var i=0;i<ttq.methods.length;i++){
+                ttq.setAndDefer(ttq,ttq.methods[i])
+              }
+              ttq.instance=function(t){
+                for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++){
+                  ttq.setAndDefer(e,ttq.methods[n])
+                }
+                return e
+              };
+              ttq.load=function(e,n){
+                var r="https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i=ttq._i||{};
+                ttq._i[e]=[];
+                ttq._i[e]._u=r;
+                ttq._t=ttq._t||{};
+                ttq._t[e]=+new Date;
+                ttq._o=ttq._o||{};
+                ttq._o[e]=n||{};
+                n=d.createElement("script");
+                n.type="text/javascript";
+                n.async=!0;
+                n.src=r+"?sdkid="+e+"&lib="+t;
+                e=d.getElementsByTagName("script")[0];
+                e.parentNode.insertBefore(n,e)
+              };
+
+              ttq.load('${TIKTOK_PIXEL_ID}');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
       </head>
 
       <body className={cn('font-sans', inter.variable)}>
+        {/* Meta noscript */}
         <noscript>
           <img
             height="1"
